5f3714af9d2ca322c4e7c76acce747126705de13
Andriy Romanov <judo.ras@gmail.com>
2016-06-25 01:00:42 -0700

    Added functionality to minify css classes provided in classesMap

--- css/css.go
+++ css/css.go
@@ -1,8 +1,8 @@
 package css
 
 import (
-	// "fmt"
 	"github.com/aymerick/douceur/parser"
+	"github.com/gorilla/css/scanner"
 )
 
 func MinifyCss(content string, classesMap map[string]string) (string, error) {
@@ -11,7 +11,25 @@ func MinifyCss(content string, classesMap map[string]string) (string, error) {
 		return "", err
 	}
 	for _, rule := range stylesheet.Rules {
-		rule.Selectors = []string{".a"}
+		selectors := make([]string, 0, 0)
+		for _, selector := range rule.Selectors {
+			s := scanner.New(selector)
+			token := s.Next()
+			if token.Type != scanner.TokenChar || token.Value != "." {
+				selectors = append(selectors, selector)
+				continue
+			}
+			token = s.Next()
+			if token.Type != scanner.TokenIdent {
+				selectors = append(selectors, selector)
+				continue
+			}
+			if classesMap[token.Value] == "" {
+				continue
+			}
+			selectors = append(selectors, "."+classesMap[token.Value])
+		}
+		rule.Selectors = selectors
 	}
 	return stylesheet.String(), nil
 }
