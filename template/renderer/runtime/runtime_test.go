b7f1b0f6912cbcbd6522c875ff36b5a0732ad6ab
Andriy Romanov <judo.ras@gmail.com>
2016-08-06 15:56:37 -0700

    Added test for variable nested value (runtime rendering)

--- template/renderer/runtime/runtime_test.go
+++ template/renderer/runtime/runtime_test.go
@@ -3,6 +3,7 @@ package runtime
 import (
 	"github.com/romanoff/hyper-ui/template/ast"
 	"testing"
+	"encoding/json"
 )
 
 func TestRenderText(t *testing.T) {
@@ -42,6 +43,32 @@ func TestRenderVariable(t *testing.T) {
 		t.Errorf("Expected not to get error while rendering variable node, but got %v", err)
 	}
 	expected := "Hello"
+	if expected != string(content) {
+		t.Errorf("Expected to get:\n%v\n,but got:\n%v\n", expected, string(content))
+	}
+}
+
+func TestRenderVariableNested(t *testing.T) {
+	tr := &ast.Tree{
+		Root: &ast.ListNode{Nodes: []ast.Node{
+			&ast.VariableNode{
+				NodeType: ast.NodeVariable,
+				Ident:    []string{".", "a", "b"},
+			},
+		}},
+	}
+	renderer := &Renderer{Tree: tr}
+	renderer.Init()
+	var context map[string]interface{}
+	if err := json.Unmarshal([]byte(`{"a": {"b": "Hello from json"}}`), &context); err != nil {
+		t.Errorf("Undefined json: %v", err)
+	}
+	// fmt.Println(context)
+	content, err := renderer.Render(context)
+	if err != nil {
+		t.Errorf("Expected not to get error while rendering variable node, but got %v", err)
+	}
+	expected := "Hello from json"
 	if expected != string(content) {
 		t.Errorf("Expected to get:\n%v\n,but got:\n%v\n", expected, string(content))
 	}
