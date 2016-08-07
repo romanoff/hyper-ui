2bf2a0ab318d4f4078e04b36c8acf631f1abb289
Andriy Romanov <judo.ras@gmail.com>
2016-08-06 18:38:36 -0700

    Started working on incrementaldom renderer

---
+++ template/renderer/incrementaldom/incrementaldom_test.go
@@ -0,0 +1,27 @@
+package incrementaldom
+
+import (
+	"github.com/romanoff/hyper-ui/template/ast"
+	"testing"
+)
+
+func TestRenderText(t *testing.T) {
+	tr := &ast.Tree{
+		Root: &ast.ListNode{Nodes: []ast.Node{
+			&ast.TextNode{
+				NodeType: ast.NodeText,
+				Content:  []byte("Hello world"),
+			},
+		}},
+	}
+	renderer := &Renderer{Tree: tr}
+	renderer.Init()
+	content, err := renderer.Render()
+	if err != nil {
+		t.Errorf("Expected not to get error while rendering text node, but got %v", err)
+	}
+	expected := "text('Hello world')\n"
+	if expected != string(content) {
+		t.Errorf("Expected to get:\n%v\n,but got:\n%v\n", expected, string(content))
+	}
+}
